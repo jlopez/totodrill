1e25e5a2fbd229cc44887831c5fb9f9ba909b1b9
Jesus Lopez <jesus@jesusla.com>
2023-12-04 02:58:36 -0800

    Initial Commit

---
+++ totodrill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  totodrill
+//
+//  Created by Jesus Lopez on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ totodrill/totodrillApp.swift
@@ -0,0 +1,17 @@
+//
+//  totodrillApp.swift
+//  totodrill
+//
+//  Created by Jesus Lopez on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct totodrillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
